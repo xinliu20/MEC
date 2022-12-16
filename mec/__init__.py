5b38da8718a9be14ffc085852f8ba0d0f963b51f
xinliu20 <109497799+xinliu20@users.noreply.github.com>
2022-12-17 07:06:57 +0800

    Initial commit

---
+++ mec/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Self-Supervised Learning via Maximum Entropy Coding
+
+    36th Conference on Neural Information Processing Systems (NeurIPS 2022)
+
+    The code is mainly based on https://github.com/facebookresearch/simsiam
+"""
